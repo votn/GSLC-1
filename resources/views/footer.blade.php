20a0864e561a66cdb3b9ebdeecb7973e1b9a1331
votn <80503124+votn@users.noreply.github.com>
2022-11-09 21:28:13 +0700

    Add files via upload

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+
+<div class="text-center bg-dark text-white">
+    <div class="pt-4 pb-4">
+        <h1>APEX LEGEND</h1>
+        <H3>2440017675 - JERRY STEFANUS SAPUTRA</H3>
+    </div>
+</div>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <a class="navbar-brand fs-1 ms-5 text-white" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif" href="#">Apex Legend</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse ms-2 ps-2 fs-4" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="/legend">Legend</a>
+        </li>
+      </ul>
+    </div>
+</nav>
